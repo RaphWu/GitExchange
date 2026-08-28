36cc3663a8ca886635b94eb72051f9f6ce441cf7
Raphael Wu <raphael0422@gmail.com>
2026-08-28 17:14:24 +0800

    20260828 Office

--- EmptyProject/Program.cs
+++ EmptyProject/Program.cs
@@ -3,65 +3,208 @@ using ACadSharp.IO;
 
 class Program
 {
-    static void Main()
+    /// <summary>
+    /// 註冊監聽原始輸入設備。
+    /// </summary>
+    /// <param name="pRawInputDevices">原始輸入裝置集。</param>
+    /// <param name="uiNumDevices">裝置集的元素個數。</param>
+    /// <param name="cbSize">原始輸入裝置資訊所佔用的位元組數。</param>
+    /// <returns>若執行成功則傳回 true，否則為 false，可透過呼叫 GetLastError 方法來取得更多關於失敗的資訊。</returns>
+    [DllImport("User32.dll", SetLastError = true)]
+    internal static extern bool RegisterRawInputDevices(
+        RawInputDevice[] pRawInputDevices,
+        uint uiNumDevices,
+        uint cbSize);
+
+    /// <summary>
+    /// 定義原始資料設備訊息。
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct RawInputDevice
     {
-        // 读取 DWG 文件
-        CadDocument document = DwgReader.Read("你的图纸.dwg");
+        /// <summary>頂層集合用法頁，介面裝置用法頁。</summary>
+        internal HidUsagePage usUsagePage;
 
-        // 统计实体数量
-        Console.WriteLine($"文档包含{document.Entities.Count}个几何实体");
+        /// <summary>頂層集合用法，即監聽設備標識。</summary>
+        internal HidUsage usUsage;
 
-        // 遍历所有图层
-        // 遍历文件夹中的所有 DWG 文件
-        foreach (string file in Directory.GetFiles("图纸文件夹", "*.dwg"))
+        /// <summary>
+        /// 模式標識，指示如何解釋處理由用法頁和用法提供的信息。<br/>
+        /// 預設為 Zero 時，作業系統會在已經註冊的應用程式視窗獲得焦點時，傳送頂級集合指定的裝置原始數據。
+        /// </summary>
+        internal RawInputDeviceFlags dwFlags;
+
+        /// <summary>與監聽裝置關聯的目標視窗句柄，如果為空，則遵循鍵盤焦點。</summary>
+        internal IntPtr hwndTarget;
+    }
+
+    public RawKeyBoard(IntPtr hwnd, bool captureOnlyInForeground)
+    {
+        var rid = new RawInputDevice[1];
+
+        rid[0].usUsagePage = HidUsagePage.GENERIC;
+        rid[0].usUsage = HidUsage.Keyboard;
+        rid[0].dwFlags =
+            (captureOnlyInForeground
+                ? RawInputDeviceFlags.UNDEFINE
+                : RawInputDeviceFlags.RIDEV_INPUTSINK)
+            | RawInputDeviceFlags.RIDEV_DEVNOTIFY;
+        rid[0].hwndTarget = hwnd;
+
+        if (!Win32API.RegisterRawInputDevices(rid, (uint)rid.Length, (uint)Marshal.SizeOf(rid[0])))
         {
-            CadDocument doc = DwgReader.Read(file);
+            throw new ApplicationException("註冊設備失敗！");
+        }
+    }
 
-            // 查找所有圆形实体
-            var circles = doc.Entities.OfType<Circle>();
-            foreach (var circle in circles)
+    protected override void WndProc(ref Message message)
+    {
+        switch (message.Msg)
+        {
+            case WinMessage.WM_INPUT:
+                {
+                    keyBoardDriver.ProcessRawInput(message.LParam);
+                }
+                break;
+            case WinMessage.WM_USB_DEVICECHANGE:
+                {
+                    keyBoardDriver.EnumerateDevices();
+                }
+                break;
+        }
+        base.WndProc(ref message);
+    }
+
+
+    [DllImport("User32.dll", SetLastError = true)]
+    internal static extern int GetRawInputData(
+        IntPtr hRawInput,
+        RawInputDataCommand command,
+        [Out] IntPtr pData,
+        [In, Out] ref int size,
+        int sizeHeader);
+
+
+
+    /// <summary>
+    /// 包含來自裝置的原始輸入
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct RAWINPUT
+    {
+        [FieldOffset(0)]
+        internal RAWMOUSE mouse;
+        [FieldOffset(0)]
+        internal RAWKEYBOARD keyboard;
+        [FieldOffset(0)]
+        internal RAWHID hid;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct RAWHID
+    {
+        public uint dwSizHid;
+        public uint dwCount;
+        public byte bRawData;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct RAWMOUSE
+    {
+        [FieldOffset(0)]
+        public ushort usFlags;
+        [FieldOffset(4)]
+        public uint ulButtons;
+        [FieldOffset(4)]
+        public ushort usButtonFlags;
+        [FieldOffset(6)]
+        public ushort usButtonData;
+        [FieldOffset(8)]
+        public uint ulRawButtons;
+        [FieldOffset(12)]
+        public int lLastX;
+        [FieldOffset(16)]
+        public int lLastY;
+        [FieldOffset(20)]
+        public uint ulExtraInformation;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct RAWKEYBOARD
+    {
+        public ushort Makecode;
+        public ushort Flags;
+        private readonly ushort Reserved;
+        public ushort VKey;
+        public uint Message;
+        public uint ExtraInformation;
+    }
+
+    public int ProcessRawInput(IntPtr hdevice)
+    {
+        if (deviceList.Count == 0) return 0;
+        var dwSize = 0;
+        Win32API.GetRawInputData(hdevice, RawInputDataCommand.RID_INPUT, IntPtr.Zero, ref dwSize, Marshal.SizeOf(typeof(Rawinputheader)));
+        if (dwSize != Win32API.GetRawInputData(hdevice, RawInputDataCommand.RID_INPUT, out _rawBuffer, ref dwSize, Marshal.SizeOf(typeof(Rawinputheader))))
+        {
+            return 1;
+        }
+        int virtualKey = _rawBuffer.data.keyboard.VKey;
+        int makeCode = _rawBuffer.data.keyboard.Makecode;
+        int flags = _rawBuffer.data.keyboard.Flags;
+        if (virtualKey == WinMessage.KEYBOARD_OVERRUN_MAKE_CODE) return 0;
+        var isE0BitSet = ((flags & WinMessage.RI_KEY_E0) != 0);
+        KeyPressEvent keyPressEvent;
+        if (deviceList.ContainsKey(_rawBuffer.header.hDevice))
+        {
+            lock (padLock)
             {
-                Console.WriteLine($"文件: {file}, 圆半径: {circle.Radius}");
+                keyPressEvent = deviceList[_rawBuffer.header.hDevice];
             }
         }
-    }
-
-    // 自定义实体处理器示例
-    public class CustomBlockTemplate : CadEntityTemplate<CustomBlock>
-    {
-// 优化大型文件处理
-public void ProcessLargeCadFile(string filePath)
-{
-    using (var reader = new DwgReader(filePath))
-    {
-        // 启用流式处理模式，减少内存占用
-        reader.Configuration.StreamingMode = true;
-
-        // 跳过不需要的实体类型，提高处理速度
-        reader.Configuration.EntitiesToSkip = new Type[]
+        else
         {
-            typeof(Hatch),
-            typeof(Mesh),
-            typeof(ImageDefinition)
-        };
-
-        // 分批次处理实体
-        CadDocument doc = reader.Read();
-
-        // 按类型分组处理，提高效率
-        var entitiesByType = doc.ModelSpace.Entities
-                            .GroupBy(e => e.GetType())
-                    .OrderBy(g => g.Count());
-
-        foreach (var group in entitiesByType)
-        {
-            Console.WriteLine($"{group.Key.Name}: {group.Count()}个");
-            ProcessEntityGroup(group.ToList());
+            return 2;
         }
-    }
-}
+        var isBreakBitSet = ((flags & WinMessage.RI_KEY_BREAK) != 0);
+        keyPressEvent.KeyPressState = isBreakBitSet ? "BREAK" : "MAKE";
+        keyPressEvent.Message = _rawBuffer.data.keyboard.Message;
+        keyPressEvent.VKeyName = KeyMapper.GetKeyName(VirtualKeyCorrection(virtualKey, isE0BitSet, makeCode)).ToUpper();
+        keyPressEvent.VKey = virtualKey;
+        if (KeyPressed != null)
+        {
+            hotkeyPressEvent.CheckKey(keyPressEvent);
+            KeyPressed(this, new RawKeyEventArg(keyPressEvent));
+        }
+        if (hotkeyPressEvent.hotActived)
+        {
+            OnHotKeyPressed(this, new RawHotKeyEventArg(hotkeyPressEvent));
+        }
+        return 0;
     }
 
-    // 使用带通知的读取器
-    CadDocument doc = DwgReader.Read("图纸.dwg", OnNotification);
+
+    static void Main()
+    {
+
+        var dwSize = 0;
+        Win32.GetRawInputData(
+            hdevice,
+            RawInputDataCommand.RID_INPUT,
+            IntPtr.Zero,
+            ref dwSize,
+            Marshal.SizeOf(typeof(Rawinputheader)));
+
+        if (dwSize == Win32.GetRawInputData(
+            hdevice,
+            RawInputDataCommand.RID_INPUT,
+            out _rawBuffer,
+            ref dwSize,
+            Marshal.SizeOf(typeof(Rawinputheader))))
+        {
+            //dosomething...
+        }
+
+
+    }
 }
